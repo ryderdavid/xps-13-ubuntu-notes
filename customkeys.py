86684638c18e65e2833844e4dc807e1b4d60e4eb
Ryder Cobean <ryderdavid@gmail.com>
2020-09-27 11:36:07 -0400

    made customkeys entirely python
    uses subprocess instead of opening bash script

--- customkeys.py
+++ customkeys.py
@@ -1,4 +1,3 @@
-# %%
 #!/usr/bin/env python
 
 """
@@ -9,21 +8,39 @@ but is reset every time an input device is added or removed.
 This Python script checks every two seconds to see whether key 66 (Caps_Lock) is Caps_Lock 
 (It should be modeswitch). If not, it shuts off capslock and runs customkeys.
 """
-
+# %%
 import subprocess
 import time
 import re
 
+def customkeys(delay=0):
+    
+    cmds = [
+        r'xmodmap -e "keycode 66 = Mode_switch"',
+        r'xmodmap -e "keycode 31 = i I Up Up"',
+        r'xmodmap -e "keycode 44 = j J Left Left"',
+        r'xmodmap -e "keycode 45 = k K Down Down"',
+        r'xmodmap -e "keycode 46 = l L Right Right"',
+        r'xmodmap -e "keycode 30 = u U Prior Prior"',
+        r'xmodmap -e "keycode 32 = o O Next Next"',
+        r'xmodmap -e "keycode 43 = h H Home Home"',
+        r'xmodmap -e "keycode 57 = n N End End"',
+        r'xmodmap -e "keycode 65 = space space Menu Menu"',
+        r'xmodmap -e "keycode 22 = BackSpace BackSpace Delete Delete"',
+    ]
+    
+    time.sleep(delay)
+    
+    for cmd in cmds:
+        subprocess.Popen(cmd, shell=True)
+    
 
-def refresh():
+def capslock_off():
     # check for whether capslock is on
     xset_outputs = subprocess.check_output(["/usr/bin/xset", "q"]).decode('utf-8').strip()
     if xset_outputs.find('Caps Lock:   on') != -1:
     # turn off capslock if it is on
         subprocess.Popen(['/usr/bin/xdotool', 'key', 'Caps_Lock'])
-    
-    # run customkeys
-    subprocess.Popen("customkeys")
     
     
 def is_capslock_capslock():
@@ -35,11 +52,17 @@ def is_capslock_capslock():
     
     else: 
         return False
-    
 
 # %%
 if __name__ == "__main__":
+    
+    # run for the first time with 10 second delay
+    capslock_off()
+    customkeys(delay=10)
+    
     while True:
         time.sleep(2)
         if is_capslock_capslock() == True:
-            refresh()
+            capslock_off()
+            customkeys(delay=0)
+
