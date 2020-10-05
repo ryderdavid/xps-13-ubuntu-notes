723729eb94d6a3b71546ad39cc8c5057ca1a7f7e
Ryder Cobean <ryderdavid@gmail.com>
2020-10-05 14:25:10 -0400

    Update customkeys.py

--- customkeys/customkeys.py
+++ customkeys/customkeys.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 """
-A tool to maintain the persistence of a simple shell script 'customkeys' which adds Pok3r
+A tool to maintain the persistence of a xmodmap commands which add Pok3r
 style key mappings via Caps Lock as a function key, with xmodmap commands. It works perfectly,
 but is reset every time an input device is added or removed. 
 
 This Python script checks every two seconds to see whether key 66 (Caps_Lock) is Caps_Lock 
 (It should be modeswitch). If not, it shuts off capslock and runs customkeys.
 """
-# %%
+
 import subprocess
 import time
 import re
@@ -33,17 +33,9 @@ def customkeys():
         subprocess.Popen(cmd, shell=True)
     
 
-def capslock_off():
-    # check for whether capslock is on
-    xset_outputs = subprocess.check_output(["/usr/bin/xset", "q"]).decode('utf-8').strip()
-    if xset_outputs.find('Caps Lock:   on') != -1:
-    # turn off capslock if it is on
-        subprocess.Popen(['/usr/bin/xdotool', 'key', 'Caps_Lock'])
-    
-    
 def is_capslock_capslock():
-    xmodmap_layout = subprocess.check_output(["xmodmap", "-pke"]).decode('utf-8').strip()
-    capslock_status = re.findall(r"keycode\s+66.+", xmodmap_layout)[0].split()
+    xmodmap_layout = subprocess.check_output('xmodmap -pke', shell=True).decode('utf-8').strip()
+    capslock_status = re.findall(r'keycode\s+66.+', xmodmap_layout)[0].split()
     
     if capslock_status[3] == 'Caps_Lock':
         return True
@@ -51,7 +43,31 @@ def is_capslock_capslock():
     else: 
         return False
 
-# %%
+
+def is_capslock_on():
+    """
+    Check whether CapsLock is toggled on
+    """
+    check = (
+        subprocess.check_output('/usr/bin/xset q', shell = True)
+            .decode('utf-8')
+            .strip()
+            .find('Caps Lock:   on')
+    )
+    
+    ret = check != -1
+    
+    return ret
+
+
+def capslock_off():
+    # check for whether capslock is on
+    if is_capslock_on() == True:
+        # turn off capslock if it is on
+        subprocess.Popen('/usr/bin/xdotool key Caps_Lock', shell=True)
+    
+    
+
 if __name__ == "__main__":
     
     # run for the first time with 10 second delay
